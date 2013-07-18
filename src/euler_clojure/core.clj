9fa842a199512ea12c15676d6536d13c9a0760da
compwron <wordymusic@gmail.com>
2013-07-17 23:05:45 -0500

    cleaning up println

--- src/euler_clojure/core.clj
+++ src/euler_clojure/core.clj
@@ -2,12 +2,8 @@
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-
-  
-
   ;; work around dangerous default behaviour in Clojure
-  (alter-var-root #'*read-eval* (constantly false))
-  (println "Hello, World!"))
+  (alter-var-root #'*read-eval* (constantly false)))
+
 (def euler1 (fn [x] (* x x)))
