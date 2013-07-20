8c4dabd8c5e1a89c665069ef7463a95290be27a4
compwron <wordymusic@gmail.com>
2013-07-19 19:17:50 -0500

    other clojure solutions from the internal forums

--- src/euler_clojure/core.clj
+++ src/euler_clojure/core.clj
@@ -29,9 +29,30 @@
 (defn euler1-dkvasnicka [n]
 	(reduce + (distinct (into (range 5 n 5) (range 3 n 3)))))
 
-
+; ; compwron
 (defn euler2-fibonacci [a b current sum]
-  ; (println "a: " a " b: " b " current: " current " sum: " sum)
 	(if (> current 1)
       (euler2-fibonacci b (+ a b) (- current 1) (+ sum b))
     (identity sum)))
+
+
+;; johnbendi clojure
+(defn last-nth [n coll]
+  (loop [coll coll]
+    (if (empty? (drop n coll)) coll (recur (next coll)))))
+
+(defn sum [coll]
+  (apply + coll))
+
+(defn sum-fibs-up-to-4M []
+  (loop [fibs [1 2]]
+    (let [last-2 (sum (last-nth 2 fibs))]
+      (if (< last-2 4000000)
+        (recur (conj fibs last-2))
+        (sum (filter even? fibs))))))
+
+; tdgs
+(defn fibo []
+ (map first (iterate (fn [[a b]] [b (+ a b)]) [0N 1N])))
+(defn solve-euler2 []
+ (reduce + (filter even? (take-while (partial > 4000000N) (fibo)))))
